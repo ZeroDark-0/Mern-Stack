8bc8a9a211a86258b9bb2ddcc198eabada9ec0d8
ZeroDark <chaitanyajain.official@gmail.com>
2026-02-05 17:53:21 +0800

    added file demo.js

---
+++ demo.js
@@ -0,0 +1,16 @@
+﻿//1.
+// const os = require('os');
+// console.log(os.uptime()); //it is the uptime of the system in Seconds
+
+//2.
+// console.log('Hello World') // this is a global syntax cuz it is not inside a function/class
+
+
+// 3.
+// const process = require('process');
+// console.log(process.argv);
+
+
+/*to store from terminal that will store the name and the section in the array
+this will run in terminal
+node demo.js "John" section3*/
