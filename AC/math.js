df9515d50daad23237ee8d62b927d4adbd766cc7
ZeroDark <chaitanyajain.official@gmail.com>
2026-03-12 19:42:10 +0800

    added folder AD

---
+++ AC/math.js
@@ -0,0 +1,26 @@
+﻿
+
+
+
+
+
+
+
+
+
+
+const https = require("http")
+const fs = require("fs")
+const server = https.createServer((req,res) => {
+    const log = `logs of this fileserver ${new Date()}\n`
+    if(req.url ==="/"){
+        res.end("hello")
+    }
+    if(req.url === "/write-file"){
+        fs.appendFileSync("logs.txt", log)
+        res.end(log)
+    }server.listen(3000, () => {
+        console.log("server is running and listening on port 3000")
+    })
+
+})
---
+++ AD/express server.js
@@ -0,0 +1,18 @@
+﻿const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+    res.send("Hello Dosto, Welcome to Express server");
+});
+app.get("/about", (req, res) => {
+    res.send("This is the about page");
+});
+app.get("/contact", (req, res) => {
+    res.send("This is the contact page");
+});
+app.get("/student/:id", (req, res) => {
+    res.send(`Welcome to Express server , Student ID: ${req.params.id}`);
+});
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
+module.exports = app;
